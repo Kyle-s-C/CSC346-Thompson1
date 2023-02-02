fe87d00464a12a46e981a0cce500cc4dc9d46a79
Kyle Thompson <kyle.m.thompson@jacks.sdstate.edu>
2023-02-02 12:52:36 -0600

    Initial Commit

---
+++ Thompson1/Program.cs
@@ -0,0 +1,16 @@
+﻿/********************************************************************
+*** NAME : Kyle Thompson                                          ***
+*** CLASS : CSc 346                                               ***
+*** ASSIGNMENT : Assignment #1                                    ***
+*** DUE DATE : 02-08-23                                           ***
+*** INSTRUCTOR : GAMRADT                                          ***
+*********************************************************************
+*** DESCRIPTION : Creating a simple program using .Net to print a ***
+***               few lines including the date, class, assignment,***
+***               and due date.                                   ***
+********************************************************************/
+
+Console.WriteLine("Kyle Thompson");
+Console.WriteLine("CSC 346 - Object Oriented Programming");
+Console.WriteLine("Assignment #1");
+Console.WriteLine("02-08-23");
